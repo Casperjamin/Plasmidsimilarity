278a5e9a1e64cd18412f2d4a079af8bba74874d0
casperjamin <casperjamin@gmail.com>
2020-03-29 23:28:51 +0200

    total refactor complete

--- scripts/plasmidread.py
+++ scripts/plasmidread.py
@@ -28,9 +28,9 @@ def seq_to_kmercount(seq, kmerdict, kmersize):
         kmer = seq[i:i+kmersize]
         # select lexographically lowest kmer between a kmer and its reverse complement
         kmer = sorted([kmer, reversecomp(kmer)])[0]
-        if kmer not in d:
-            d[kmer] = 0
-        d[kmer] += 1
+        if kmer not in kmerdict:
+            kmerdict[kmer] = 0
+        kmerdict[kmer] += 1
     return kmerdict
 
 
@@ -53,10 +53,6 @@ def generate_output(kmer_dataframe, output_file, kmersize):
     kmer_dataframe.columns = [output_file.split("/")[-1]]
     kmer_dataframe = kmer_dataframe.T
     kmer_dataframe.to_hdf(f"{output_file}_{kmersize}.hdf", key = 'df', format = 'fixed')
-    print(f"wrote kmercounts of all sequences from the file {input_file}\n ")
-
-
-
 
 
 def kmercount(input_file, output_file, kmersize = 31, circular = False):
@@ -64,6 +60,7 @@ def kmercount(input_file, output_file, kmersize = 31, circular = False):
     reads fasta file and return a hdf file containing a pandas DataFrame
     with the kmer-counts per fasta entry
     """
+    inputfilename = input_file.split('/')[-1].strip('.fasta')
     if multiplecontigs(input_file):
         # assesment of circular plasmids is considered false if more than 1 contig is present
         circular = False
@@ -81,6 +78,9 @@ def kmercount(input_file, output_file, kmersize = 31, circular = False):
         overlapping_sequence = overlapper(sequence)
         kmerdict = seq_to_kmercount(overlapping_sequence, kmerdict = kmerdict, kmersize = kmersize)
 
-    df = pd.DataFrame(kmerdict)
+    series = pd.Series(kmerdict, name = inputfilename)
+    df = pd.DataFrame(series)
+    print(df.head())
+    print(input_file)
     print(f"done counting kmers of {input_file}\t ")
     generate_output(df, output_file = output_file, kmersize = kmersize)
