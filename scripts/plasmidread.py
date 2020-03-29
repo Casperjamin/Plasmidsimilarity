a8d66d46a019b2d6532a68765f853de2584c7881
casperjamin <casperjamin@gmail.com>
2020-03-29 23:16:05 +0200

    upgrading to hopefully more readable code

--- scripts/plasmidread.py
+++ scripts/plasmidread.py
@@ -1,68 +1,85 @@
 from Bio import SeqIO
 import pandas as pd
 
-class plasmid:
-    def __init__(self, sequence, name):
-        self.sequence = sequence.upper()
-        self.name = name
-    def seq(self):
-        return self.sequence
-
-    def sequencekmercount(self, k = 31):
-        #generate kmercount of plasmid DNA sequence
-        d = {}
-        seq = self.sequence
-        length = len(seq) - k + 1
-        for i in range(length):
-            kmer = seq[i:i+k]
-            #selects lexographically lowest kmer between a kmer and its reverse complement
-            kmer = sorted([kmer, reversecomp(kmer)])[0]
-            if kmer not in d:
-                d[kmer] = 0
-            d[kmer] += 1
-        return d
-
-
-revcomp = {
-    "A":"T",
-    "C":"G",
-    "G":"C",
-    "T":"A",
-    "N":"N"
-    }
-
-
 def reversecomp(inputseq):
-    #function to generate reverse complementary of DNA seq
+    # generate reverse complementary of DNA seq
+    revcomp = {
+        "A":"T",
+        "C":"G",
+        "G":"C",
+        "T":"A",
+        "N":"N"}
 
     seq = inputseq[::-1]
     revcomstring = ""
     for i in seq:
         revcomstring += revcomp[i]
-
     return revcomstring
 
 
-
-
-def kmercount(input_file, output_file, kmersize = 31):
-    """reads fasta file and return a pickle file containing a pandas DataFrame
-    with the kmer-counts per fasta entry
+def seq_to_kmercount(seq, kmerdict, kmersize):
     """
+    DNA sequence and count number of kmers in the dictionary, and return this dictionary
+    """
+    seq = str(seq).upper()
 
-    kmerdic = {}
-    for i in SeqIO.parse(input_file, 'fasta'):
-        p = plasmid(str(i.seq), i.id)
-        count = p.sequencekmercount(kmersize)
-        kmerdic[i.id] = count
+    length = len(seq) - kmersize + 1
+    for i in range(length):
+        kmer = seq[i:i+kmersize]
+        # select lexographically lowest kmer between a kmer and its reverse complement
+        kmer = sorted([kmer, reversecomp(kmer)])[0]
+        if kmer not in d:
+            d[kmer] = 0
+        d[kmer] += 1
+    return kmerdict
 
-        print(f"done counting kmers of {input_file}\t ")
 
-    df = pd.DataFrame(kmerdic)
-    df.head()
-    df.columns = [output_file.split("/")[-1]]
-    df = df.T
-    df.to_hdf(f"{output_file}_{kmersize}.hdf", key = 'df', format = 'fixed')
+def multiplecontigs(inputfasta):
+    """
+    returns True if more than 1 contig is found in file; otherwise return False
+    """
+    count = 0
+    for i in SeqIO.parse(inputfasta, 'fasta'):
+        count += 1
+        if count > 1:
+            return True
+    return False
+
+
+def generate_output(kmer_dataframe, output_file, kmersize):
+    """
+    take kmer_dataframe of dictionaries of kmercounts, generate output files
+    """
+    kmer_dataframe.columns = [output_file.split("/")[-1]]
+    kmer_dataframe = df.T
+    kmer_dataframe.to_hdf(f"{output_file}_{kmersize}.hdf", key = 'df', format = 'fixed')
     print(f"wrote kmercounts of all sequences from the file {input_file}\n ")
 
 
+
+
+
+def kmercount(input_file, output_file, kmersize = 31, circular = False):
+    """
+    reads fasta file and return a hdf file containing a pandas DataFrame
+    with the kmer-counts per fasta entry
+    """
+    if multiplecontigs(input_file):
+        # assesment of circular plasmids is considered false if more than 1 contig is present
+        circular = False
+
+    kmerdict = {}
+
+    number_of_contigs = 0
+    for i in SeqIO.parse(input_file, 'fasta'):
+        sequence = i.seq
+        number_of_contigs += 1
+        kmerdict = seq_to_kmercount(sequence, kmerdict = kmerdict, kmersize = kmersize)
+
+    if number_of_contigs == 1 & circular == True:
+        overlapping_sequence = overlapper(sequence)
+        kmerdict = seq_to_kmercount(overlapping_sequence, kmerdict = kmerdict, kmersize = kmersize)
+
+    df = pd.DataFrame(kmerdict)
+    print(f"done counting kmers of {input_file}\t ")
+    generate_output(df, output_file = output_file, kmersize = kmersize)
