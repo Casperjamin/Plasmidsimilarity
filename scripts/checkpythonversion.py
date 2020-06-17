f8e438d561bb5a49e1f89de4057234c24ad88bce
casperjamin <casperjamin@gmail.com>
2020-06-17 12:08:12 +0200

    write python version to stderr

--- scripts/checkpythonversion.py
+++ scripts/checkpythonversion.py
@@ -2,6 +2,6 @@ import sys
 
 def check_right_version():
     if sys.version_info[0] > 2 and sys.version_info[1] > 5:
-        print(f'Python version is ok: {sys.version}')
+        sys.stderr.write(f'Python version is ok: {sys.version}\n')
     else:
         raise PythonVersionException(f'This tool requires Python 3.6 or higher. \t You are using {sys.version}')
