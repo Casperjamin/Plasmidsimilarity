0bb43f08eb35b8ad4cdbe9b71b3aafe6ef2ea249
casperjamin <casperjamin@gmail.com>
2020-06-29 09:09:44 +0200

    setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     version="v0.4.0",
     author="Casper Jamin",                                               
     author_email="casperjamin@gmail.com",                                       
-    description="Clustering plasmids based on their k-mer content"",            
+    description="Clustering plasmids based on their k-mer content",            
     url="https://github.com/casperjamin/plasmidsimilarity",                     
     install_requires=['numpy', 'biopython','pandas', 'networkx','pytables', 'scipy', 'matplotlib', 'seaborn'],
     python_requires='>=3.6'                                                     
