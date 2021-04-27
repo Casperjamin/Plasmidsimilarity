9a56bab8a251bbecf7e2648c8aaa234eae30e52a
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2021-04-27 22:02:43 +0200

    bump version

--- setup.py
+++ setup.py
@@ -59,6 +59,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/casperjamin/Plasmidsimilarity',
-    version='0.0.1',
+    version='0.6.0',
     zip_safe=False,
 )
