07e71f13d6c993156d3f84132dbdf1c9d3ef39b2
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2021-05-04 11:58:25 +0200

    first set of testing

--- tests/test_Plasmidsimilarity.py
+++ tests/test_Plasmidsimilarity.py
@@ -2,10 +2,9 @@
 
 """Tests for `Plasmidsimilarity` package."""
 
-
 import unittest
-
-from Plasmidsimilarity import Plasmidsimilarity
+import pytest
+from Plasmidsimilarity.scripts.describe import nucl_count
 
 
 class TestPlasmidsimilarity(unittest.TestCase):
@@ -13,9 +12,18 @@ class TestPlasmidsimilarity(unittest.TestCase):
 
     def setUp(self):
         """Set up test fixtures, if any."""
-
+        self.plasmidseq = "ACGTACGTACGTACGT"
+        self.plasmidseqwithN = "ACTGNNNN"
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+
+    def test_describe(self):
         """Test something."""
+        # describe.py test
+        assert nucl_count(self.plasmidseq)['A'] == 4
+        assert nucl_count(self.plasmidseq)['C'] == 4
+        assert nucl_count(self.plasmidseq)['G'] == 4
+        assert nucl_count(self.plasmidseq)['T'] == 4
+        assert nucl_count(self.plasmidseq)['N'] == 0
+        assert nucl_count(self.plasmidseqwithN)['N'] == 4
