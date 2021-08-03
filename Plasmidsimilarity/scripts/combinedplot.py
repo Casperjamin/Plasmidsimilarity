8313c1067ad7781de33d798e4200a6efb24df9bd
Casper Jamin <34744774+Casperjamin@users.noreply.github.com>
2021-08-03 09:29:05 +0200

    plot adjustments to better visualise short leaftips

--- Plasmidsimilarity/scripts/combinedplot.py
+++ Plasmidsimilarity/scripts/combinedplot.py
@@ -14,6 +14,12 @@ def generateplot(abricate, distancematrix, output):
     geno = pd.read_csv(abricate, sep='\t', index_col=0)
     dist = pd.read_csv(distancematrix, sep='\t', index_col=0)
 
+    # make new df to put in all samples
+
+    genotemp = pd.DataFrame(index = dist.index, columns = geno.columns, data = geno.to_dict())
+    geno = genotemp.fillna(0)
+
+
     height = minsize(len(geno) * 0.6, 6)
     width = minsize(len(geno.columns) * 0.4, 10) * 2
     plot, axes = plt.subplots(1, 2, figsize=[width, height])
@@ -33,6 +39,17 @@ def generateplot(abricate, distancematrix, output):
             linewidth=0.01,
             linecolor='black')
     axes[0].set_ylabel("")
+    axes[1].set_xlabel("K-mer based Jaccard dissimilarity")
     axes[1].get_yaxis().set_visible(False)
-    plt.subplots_adjust(wspace=0)
+
+    # hide left border of dendroplot to better show leaf tips
+    left_side = axes[1].spines["left"]
+    left_side.set_visible(False)
+        
+    # show lines properly around heatmap
+    for _, spine in axes[0].spines.items():
+        spine.set_visible(True)
+
+
+    plt.subplots_adjust(wspace=0.005)
     plt.savefig(output, bbox_inches='tight')
